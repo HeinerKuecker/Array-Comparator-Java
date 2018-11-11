9068095dfc80058565040a3e257bf3e8c46fbcfc
HeinerKuecker <mail@heinerkuecker.de>
2018-11-11 21:43:16 +0100

    Dim3IntArrayComparator wip

---
+++ ARRAY_COMPARATOR/src/de/heinerkuecker/comparator/array/prm/mlt/Dim3IntArrayComparator.java
@@ -0,0 +1,42 @@
+package de.heinerkuecker.comparator.array.prm.mlt;
+
+import java.util.Comparator;
+
+import de.heinerkuecker.comparator.array.ComparatorArrayComparator;
+import de.heinerkuecker.comparator.array.Nulls;
+
+/**
+ * {@link Comparator} for three
+ * dimensional arrays whose
+ * elements are primitive int.
+ *
+ * @author Heiner K&uuml;cker
+ */
+public class Dim3IntArrayComparator
+extends ComparatorArrayComparator<int[][]>
+{
+
+	/**
+	 * Constructor.
+	 *
+	 * @param arrayNulls Control handling of null arrays to sort
+	 * @param subArrayNulls Control handling of null sub arrays to sort
+	 * @param subSubArrayNulls Control handling of null sub sub arrays to sort
+	 * @param elementNulls Control handling of null elements in arrays to sort
+	 */
+	public Dim3IntArrayComparator(
+			final Nulls arrayNulls ,
+			final Nulls subArrayNulls ,
+			final Nulls subSubArrayNulls )
+	{
+		// TODO test
+		// TODO null handling params correct???
+		super(
+				arrayNulls ,
+				subArrayNulls ,
+				new Dim2IntArrayComparator(
+						Nulls.FORBIDDEN ,
+						subSubArrayNulls ) );
+	}
+
+}
