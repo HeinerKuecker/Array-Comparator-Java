cf90d91a099af5910483c7790f2b56f15e508d3c
HeinerKuecker <mail@heinerkuecker.de>
2018-11-12 06:52:56 +0100

    more comparators and more tests

---
+++ ARRAY_COMPARATOR/src/de/heinerkuecker/comparator/array/mlt/Dim3ComparableArrayComparator.java
@@ -0,0 +1,47 @@
+package de.heinerkuecker.comparator.array.mlt;
+
+import java.util.Comparator;
+
+import de.heinerkuecker.comparator.array.ComparableArrayComparator;
+import de.heinerkuecker.comparator.array.ComparatorArrayComparator;
+import de.heinerkuecker.comparator.array.Nulls;
+
+/**
+ * {@link Comparator} for two dimensional
+ * arrays whose elements are {@link Comparable}.
+ *
+ * @param <T> Element type of arrays to compare
+ * @author Heiner K&uuml;cker
+ */
+public class Dim3ComparableArrayComparator<T extends Comparable<T>>
+extends ComparatorArrayComparator<T[][]>
+{
+
+	/**
+	 * Constructor.
+	 *
+	 * @param arrayNulls Control handling of null arrays to sort
+	 * @param subArrayNulls Control handling of null sub arrays to sort
+	 * @param subSubArrayNulls Control handling of null sub sub arrays to sort
+	 * @param elementNulls Control handling of null elements in arrays to sort
+	 */
+	public Dim3ComparableArrayComparator(
+			final Nulls arrayNulls ,
+			final Nulls subArrayNulls ,
+			final Nulls subSubArrayNulls ,
+			final Nulls elementNulls )
+	{
+		// TODO test
+		// TODO null handling params correct???
+		super(
+				arrayNulls ,
+				subArrayNulls ,
+				ComparatorArrayComparator.<T[]>newComparatorArrayComparator(
+						Nulls.FORBIDDEN ,
+						subSubArrayNulls ,
+						ComparableArrayComparator.<T>newComparableArrayComparator(
+								Nulls.FORBIDDEN ,
+								elementNulls ) ) );
+	}
+
+}
