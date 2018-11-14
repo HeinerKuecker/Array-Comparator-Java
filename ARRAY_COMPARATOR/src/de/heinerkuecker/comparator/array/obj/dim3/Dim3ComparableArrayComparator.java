01bb0a6330d9e9020642fa56d163ebcb625730d3
HeinerKuecker <mail@heinerkuecker.de>
2018-11-14 04:03:38 +0100

    work in progress

--- ARRAY_COMPARATOR/src/de/heinerkuecker/comparator/array/obj/dim3/Dim3ComparableArrayComparator.java
+++ ARRAY_COMPARATOR/src/de/heinerkuecker/comparator/array/obj/dim3/Dim3ComparableArrayComparator.java
@@ -3,8 +3,8 @@ package de.heinerkuecker.comparator.array.obj.dim3;
 import java.util.Comparator;
 
 import de.heinerkuecker.comparator.array.Nulls;
-import de.heinerkuecker.comparator.array.obj.dim1.ComparableArrayComparator;
 import de.heinerkuecker.comparator.array.obj.dim1.ComparatorArrayComparator;
+import de.heinerkuecker.comparator.array.obj.dim2.Dim2ComparableArrayComparator;
 
 /**
  * {@link Comparator} for two dimensional
@@ -17,31 +17,52 @@ public class Dim3ComparableArrayComparator<T extends Comparable<T>>
 extends ComparatorArrayComparator<T[][]>
 {
 
-	/**
-	 * Constructor.
-	 *
-	 * @param arrayNulls Control handling of null arrays to sort
-	 * @param subArrayNulls Control handling of null sub arrays to sort
-	 * @param subSubArrayNulls Control handling of null sub sub arrays to sort
-	 * @param elementNulls Control handling of null elements in arrays to sort
-	 */
-	public Dim3ComparableArrayComparator(
-			final Nulls arrayNulls ,
-			final Nulls subArrayNulls ,
-			final Nulls subSubArrayNulls ,
-			final Nulls elementNulls )
-	{
-		// TODO test
-		// TODO null handling params correct???
-		super(
-				arrayNulls ,
-				subArrayNulls ,
-				ComparatorArrayComparator.<T[]>newComparatorArrayComparator(
-						Nulls.FORBIDDEN ,
-						subSubArrayNulls ,
-						ComparableArrayComparator.<T>newComparableArrayComparator(
-								Nulls.FORBIDDEN ,
-								elementNulls ) ) );
-	}
+    /**
+     * Constructor.
+     *
+     * @param arrayNulls Control handling of null arrays to sort
+     * @param subArrayNulls Control handling of null sub arrays to sort
+     * @param subSubArrayNulls Control handling of null sub sub arrays to sort
+     * @param elementNulls Control handling of null elements in arrays to sort
+     */
+    public Dim3ComparableArrayComparator(
+            final Nulls arrayNulls ,
+            final Nulls subArrayNulls ,
+            final Nulls subSubArrayNulls ,
+            final Nulls elementNulls )
+    {
+        // TODO test
+        // TODO null handling params correct???
+        super(
+                arrayNulls ,
+                subArrayNulls ,
+                //ComparatorArrayComparator.<T[]>newComparatorArrayComparator(
+                //        Nulls.FORBIDDEN ,
+                //        subSubArrayNulls ,
+                //        ComparableArrayComparator.<T>newComparableArrayComparator(
+                //                Nulls.FORBIDDEN ,
+                //                elementNulls ) ) );
+                new Dim2ComparableArrayComparator<T>(
+                        // null arrays already handled by one level higher
+                        Nulls.FORBIDDEN ,
+                        subSubArrayNulls ,
+                        elementNulls ) );
+    }
+
+    /**
+     * @see Object#toString()
+     */
+    @Override
+    public String toString()
+    {
+        return
+                this.getClass().getSimpleName() +
+                "[" +
+                "arrayNulls=" + this.arrayNulls + ", " +
+                "subArrayNulls=" + this.elementNulls + ", " +
+                "subSubArrayNulls=" + ( (Dim2ComparableArrayComparator<?>) this.comparator ).arrayNulls + ", " +
+                "elementNulls=" + ( (Dim2ComparableArrayComparator<?>) this.comparator ).elementNulls +
+                "]";
+    }
 
 }
--- ARRAY_COMPARATOR/src/de/heinerkuecker/comparator/array/obj/dim3/Dim3ComparatorArrayComparator.java
+++ ARRAY_COMPARATOR/src/de/heinerkuecker/comparator/array/obj/dim3/Dim3ComparatorArrayComparator.java
@@ -4,6 +4,7 @@ import java.util.Comparator;
 
 import de.heinerkuecker.comparator.array.Nulls;
 import de.heinerkuecker.comparator.array.obj.dim1.ComparatorArrayComparator;
+import de.heinerkuecker.comparator.array.obj.dim2.Dim2ComparatorArrayComparator;
 
 /**
  * {@link Comparator} for three dimensional
@@ -17,34 +18,57 @@ public class Dim3ComparatorArrayComparator<T extends Comparable<T>>
 extends ComparatorArrayComparator<T[][]>
 {
 
-	/**
-	 * Constructor.
-	 *
-	 * @param arrayNulls Control handling of null arrays to sort
-	 * @param subArrayNulls Control handling of null sub arrays to sort
-	 * @param subSubArrayNulls Control handling of null sub sub arrays to sort
-	 * @param elementNulls Control handling of null elements in arrays to sort
-	 * @param comparator Comparator to compare array elements
-	 */
-	public Dim3ComparatorArrayComparator(
-			final Nulls arrayNulls ,
-			final Nulls subArrayNulls ,
-			final Nulls subSubArrayNulls ,
-			final Nulls elementNulls ,
-			final Comparator<? super T> comparator )
-	{
-		// TODO test
-		// TODO null handling params correct???
-		super(
-				arrayNulls ,
-				subArrayNulls ,
-				ComparatorArrayComparator.<T[]>newComparatorArrayComparator(
-						Nulls.FORBIDDEN ,
-						subSubArrayNulls ,
-						ComparatorArrayComparator.<T>newComparatorArrayComparator(
-								Nulls.FORBIDDEN ,
-								elementNulls ,
-								comparator ) ) );
-	}
+    /**
+     * Constructor.
+     *
+     * @param arrayNulls Control handling of null arrays to sort
+     * @param subArrayNulls Control handling of null sub arrays to sort
+     * @param subSubArrayNulls Control handling of null sub sub arrays to sort
+     * @param elementNulls Control handling of null elements in arrays to sort
+     * @param comparator Comparator to compare array elements
+     */
+    public Dim3ComparatorArrayComparator(
+            final Nulls arrayNulls ,
+            final Nulls subArrayNulls ,
+            final Nulls subSubArrayNulls ,
+            final Nulls elementNulls ,
+            final Comparator<? super T> comparator )
+    {
+        // TODO test
+        // TODO null handling params correct???
+        super(
+                arrayNulls ,
+                subArrayNulls ,
+                //ComparatorArrayComparator.<T[]>newComparatorArrayComparator(
+                //        Nulls.FORBIDDEN ,
+                //        subSubArrayNulls ,
+                //        ComparatorArrayComparator.<T>newComparatorArrayComparator(
+                //                Nulls.FORBIDDEN ,
+                //                elementNulls ,
+                //                comparator ) ) );
+                new Dim2ComparatorArrayComparator<T>(
+                        // null arrays already handled by one level higher
+                        Nulls.FORBIDDEN ,
+                        subSubArrayNulls ,
+                        elementNulls ,
+                        comparator ) );
+    }
+
+    /**
+     * @see Object#toString()
+     */
+    @Override
+    public String toString()
+    {
+        return
+                this.getClass().getSimpleName() +
+                "[" +
+                "arrayNulls=" + this.arrayNulls + ", " +
+                "subArrayNulls=" + this.elementNulls + ", " +
+                "subSubArrayNulls=" + ( (Dim2ComparatorArrayComparator<?>) this.comparator ).arrayNulls + ", " +
+                "elementNulls=" + ( (Dim2ComparatorArrayComparator<?>) this.comparator ).elementNulls +
+                "comparator=" + ( (Dim2ComparatorArrayComparator<?>) this.comparator ).comparator + ", " +
+                "]";
+    }
 
 }
