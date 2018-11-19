f94a226ffea33925767c31f1c9127fb9d6603d0d
HeinerKuecker <mail@heinerkuecker.de>
2018-11-19 08:14:06 +0100

    more comparators for primitive three-dimenaional arrays

---
+++ ARRAY_COMPARATOR/src/de/heinerkuecker/comparator/array/prm/dim3/Dim3CharArrayComparator.java
@@ -0,0 +1,59 @@
+package de.heinerkuecker.comparator.array.prm.dim3;
+
+import java.util.Comparator;
+
+import de.heinerkuecker.comparator.array.Nulls;
+import de.heinerkuecker.comparator.array.obj.dim1.ComparatorArrayComparator;
+import de.heinerkuecker.comparator.array.prm.dim2.Dim2CharArrayComparator;
+
+/**
+ * {@link Comparator} for
+ * three-dimensional arrays
+ * whose elements are
+ * primitive char.
+ *
+ * @author Heiner K&uuml;cker
+ */
+public class Dim3CharArrayComparator
+extends ComparatorArrayComparator<char[][]>
+{
+
+    /**
+     * Constructor.
+     *
+     * @param arrayNulls Control handling of null arrays to sort
+     * @param subArrayNulls Control handling of null sub arrays to sort
+     * @param subSubArrayNulls Control handling of null sub sub arrays to sort
+     */
+    public Dim3CharArrayComparator(
+            final Nulls arrayNulls ,
+            final Nulls subArrayNulls ,
+            final Nulls subSubArrayNulls )
+    {
+        // TODO test
+        // TODO null handling params correct???
+        super(
+                arrayNulls ,
+                subArrayNulls ,
+                new Dim2CharArrayComparator(
+                        // null arrays already handled by one level higher
+                        Nulls.FORBIDDEN ,
+                        subSubArrayNulls ) );
+    }
+
+    /**
+     * @see Object#toString()
+     */
+    @Override
+    public String toString()
+    {
+        return
+                this.getClass().getSimpleName() +
+                "[" +
+                "arrayNulls=" + this.arrayNulls + ", " +
+                "subArrayNulls=" + this.elementNulls + ", " +
+                "subSubArrayNulls=" + ( (Dim2CharArrayComparator) this.comparator ).elementNulls +
+                "]";
+    }
+
+}
