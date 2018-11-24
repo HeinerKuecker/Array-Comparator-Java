c630b6c5e975816e773e45c51214aff65dc01e0a
HeinerKuecker <mail@heinerkuecker.de>
2018-11-24 11:02:33 +0100

    javadoc

--- ARRAY_COMPARATOR/src/de/heinerkuecker/comparator/array/Nulls.java
+++ ARRAY_COMPARATOR/src/de/heinerkuecker/comparator/array/Nulls.java
@@ -3,27 +3,29 @@ package de.heinerkuecker.comparator.array;
 /**
  * {@link Enum} to control
  * handling of null arrays
- * to sort or of null elements
- * in arrays to sort.
+ * or of null elements
+ * in arrays to compare.
  *
  * @author Heiner K&uuml;cker
  */
 public enum Nulls
 {
     /**
-     * Handle null arrays or
-     * null elements as lesser.
+     * Handle null arrays or null elements
+     * as lesser than
+     * existing arrays or elements.
      */
     LESSER ,
 
     /**
-     * Handle null arrays or
-     * null elements as greater.
+     * Handle null arrays or null elements
+     * as greater than
+     * existing arrays or elements.
      */
     GREATER ,
 
     /**
-     * Null arrays or elements forbidden,
+     * Null arrays or elements are forbidden,
      * an {@link NullPointerException}
      * will be throwed.
      */
